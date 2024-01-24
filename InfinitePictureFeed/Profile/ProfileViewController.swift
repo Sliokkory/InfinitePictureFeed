7462c0653078db686f4229269aa8a18de4909323
sliokkory <sliokkory@gmail.com>
2024-01-24 14:41:12 +0300

    fix: corrected comments from the reviewer

--- InfinitePictureFeed/Profile/ProfileViewController.swift
+++ InfinitePictureFeed/Profile/ProfileViewController.swift
@@ -15,7 +15,6 @@ final class ProfileViewController: UIViewController {
     
     private let userName: UILabel = {
         let username = UILabel()
-        username.translatesAutoresizingMaskIntoConstraints = false
         username.textAlignment = .left
         username.text = "Екатерина Новикова"
         username.textColor = .white
@@ -25,7 +24,6 @@ final class ProfileViewController: UIViewController {
     
     private let userLoginName: UILabel = {
         let userLoginName = UILabel()
-        userLoginName.translatesAutoresizingMaskIntoConstraints = false
         userLoginName.textAlignment = .left
         userLoginName.text = "@ekaterina_nov"
         userLoginName.textColor = UIColor(rgb: 0xAEAFB4)
@@ -35,7 +33,6 @@ final class ProfileViewController: UIViewController {
     
     private let userMessage: UILabel = {
         let userMessage = UILabel()
-        userMessage.translatesAutoresizingMaskIntoConstraints = false
         userMessage.textAlignment = .left
         userMessage.text = "Hello, world!"
         userMessage.textColor = .white
@@ -46,35 +43,35 @@ final class ProfileViewController: UIViewController {
     private let exitButton:UIButton = {
         let exitButton = UIButton(type: .custom)
         let exitImage = UIImage(named: "exit")
-        exitButton.translatesAutoresizingMaskIntoConstraints = false
         exitButton.setTitle("", for: .normal)
         exitButton.setImage(exitImage, for: .normal)
         return exitButton
     }()
     
     private func setupViews() {
-        let views = [profileImage, userName, userLoginName, userMessage, exitButton]
-        views.forEach {
+        [profileImage, userName, userLoginName, userMessage, exitButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
     }
     
     private func setupConstraint() {
-        profileImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 76).isActive = true
-        profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        NSLayoutConstraint.activate([
+            profileImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 76),
+            profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
 
-        userName.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 8).isActive = true
-        userName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+            userName.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 8),
+            userName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
 
-        userLoginName.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 8).isActive = true
-        userLoginName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+            userLoginName.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 8),
+            userLoginName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
 
-        userMessage.topAnchor.constraint(equalTo: userLoginName.bottomAnchor, constant: 8).isActive = true
-        userMessage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        
-        exitButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 89).isActive = true
-        exitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+            userMessage.topAnchor.constraint(equalTo: userLoginName.bottomAnchor, constant: 8),
+            userMessage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            exitButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 89),
+            exitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
 }
 
