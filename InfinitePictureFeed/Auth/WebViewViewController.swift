1a6d9f7c5b5fb04a428f97a7cd9b4058e415a8a6
sliokkory <sliokkory@gmail.com>
2024-01-29 19:29:10 +0300

    fix: some fixes after review

--- InfinitePictureFeed/Auth/WebViewViewController.swift
+++ InfinitePictureFeed/Auth/WebViewViewController.swift
@@ -7,16 +7,13 @@ protocol WebViewViewControllerDelegate: AnyObject {
 }
 
 final class WebViewViewController: UIViewController {
+    weak var delegate: WebViewViewControllerDelegate?
+    
     @IBOutlet private var webView: WKWebView!
     @IBOutlet private var progressView: UIProgressView!
 
-    weak var delegate: WebViewViewControllerDelegate?
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        print(documentsURL)
 
         webView.navigationDelegate = self
         webView.addObserver(
@@ -25,14 +22,19 @@ final class WebViewViewController: UIViewController {
             options: .new,
             context: nil)
 
-        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        guard var urlComponents = URLComponents(string: unsplashAuthorizeURLString) else {
+            return
+        }
         urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: AccessKey),
-            URLQueryItem(name: "redirect_uri", value: RedirectURI),
+            URLQueryItem(name: "client_id", value: accessKey),
+            URLQueryItem(name: "redirect_uri", value: redirectURI),
             URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: AccessScope)
+            URLQueryItem(name: "scope", value: accessScope)
         ]
-        let url = urlComponents.url!
+        
+        guard let url = urlComponents.url else {
+            return
+        }
 
         let request = URLRequest(url: url)
         webView.load(request)
@@ -50,15 +52,6 @@ final class WebViewViewController: UIViewController {
             super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
         }
     }
-
-    private func updateProgress() {
-        progressView.progress = Float(webView.estimatedProgress)
-        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
-    }
-
-    @IBAction private func didTapBackButton(_ sender: Any?) {
-        delegate?.webViewViewControllerDidCancel(self)
-    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -75,6 +68,15 @@ final class WebViewViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
+    }
+
+    private func updateProgress() {
+        progressView.setProgress(Float(webView.estimatedProgress), animated: true)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
+
+    @IBAction private func didTapBackButton(_ sender: Any?) {
+        delegate?.webViewViewControllerDidCancel(self)
     }
 }
 
--- InfinitePictureFeed/Constants.swift
+++ InfinitePictureFeed/Constants.swift
@@ -1,11 +1,8 @@
 import Foundation
 
-let AccessKey = "0IWSEo7XPkIJ5qeXImqt8SDDLu4p_mobtrfyygysTyI"
-let SecretKey = "gwKNwF-04FVnXfXBESgDhij30Q7i_9_4F75vZ4cg4W8"
-let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
-let AccessScope = "public+read_user+write_likes"
-let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
-let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
-let GetTockenURL = "https://unsplash.com/oauth/token"
-let ResponseType = "code"
-
+let accessKey = "0IWSEo7XPkIJ5qeXImqt8SDDLu4p_mobtrfyygysTyI"
+let secretKey = "gwKNwF-04FVnXfXBESgDhij30Q7i_9_4F75vZ4cg4W8"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
